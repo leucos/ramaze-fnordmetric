a14ba20a3c2405706bde5cc4b6b13819c7c39112
Michel Blanc <mb@mbnet.fr>
2013-03-07 19:40:38 +0100

    Fixes coverall spec

--- spec/helper.rb
+++ spec/helper.rb
@@ -1,5 +1,6 @@
-require File.expand_path('../../lib/ramaze/helper/fnordmetric', __FILE__)
-require File.expand_path('../../lib/ramaze/helper/fnordmetric/spec/helper', __FILE__)
 require 'coveralls'
 Coveralls.wear!
+
+require File.expand_path('../../lib/ramaze/helper/fnordmetric', __FILE__)
+require File.expand_path('../../lib/ramaze/helper/fnordmetric/spec/helper', __FILE__)
 
